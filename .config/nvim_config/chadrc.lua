d617c99a60ebb146f7d92038cc53005db4ff6789
Daze <dazehere@yandex.com>
2022-07-09 16:15:58 +0545

    Dashboard enabled and .stylua config added

--- .config/nvim_config/chadrc.lua
+++ .config/nvim_config/chadrc.lua
@@ -3,54 +3,62 @@ local M = {}
 -- make sure you maintain the structure of `core/default_config.lua` here,
 
 M.ui = {
-   theme = "onedark",
+  theme = "onedark",
+  statusline = {
+    separator_style = "block", -- default/round/block/arrow
+    config = "%!v:lua.require('ui.statusline').run()",
+    override = {},
+  },
 }
 
 -- custom.mappings
 
 M.mappings = {
-   disabled = {
-      n = {
-         ["<leader>e"] = "",
-         ["<leader>rn"] = "",
-      },
-   },
-   nvimtree = { n = { ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" } } },
-   neoformat = { n = { ["<leader>fm"] = { "<cmd> Neoformat <CR>", "Format the current file" } } },
-   comment = {
-      -- toggle comment in both modes
-      n = {
-         ["<leader>c<leader>"] = {
-            function()
-               require("Comment.api").toggle_current_linewise()
-            end,
+  disabled = {
+    n = {
+      ["<leader>e"] = "",
+      ["<leader>rn"] = "",
+    },
+  },
+  nvimtree = { n = { ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" } } },
+  neoformat = { n = { ["<leader>fm"] = { "<cmd> Neoformat <CR>", "Format the current file" } } },
+  comment = {
+    -- toggle comment in both modes
+    n = {
+      ["<leader>c<leader>"] = {
+        function()
+          require("Comment.api").toggle_current_linewise()
+        end,
 
-            "蘒  toggle comment",
-         },
+        "蘒  toggle comment",
       },
+    },
 
-      v = {
-         ["<leader>c<leader>"] = {
-            "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>",
-            "蘒  toggle comment",
-         },
+    v = {
+      ["<leader>c<leader>"] = {
+        "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<CR>",
+        "蘒  toggle comment",
       },
-   },
+    },
+  },
 }
 
 -- Custom Plugins
 M.plugins = {
-   user = {
-      ["sbdchd/neoformat"] = {},
-      ["tpope/vim-surround"] = {},
-      ["tpope/vim-fugitive"] = {},
-   },
+  user = {
+    ["sbdchd/neoformat"] = {},
+    ["tpope/vim-surround"] = {},
+    ["tpope/vim-fugitive"] = {},
+    ["goolord/alpha-nvim"] = {
+      disable = false,
+    },
+  },
 
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.plugins.lspconfig",
-      },
-   },
+  options = {
+    lspconfig = {
+      setup_lspconf = "custom.plugins.lspconfig",
+    },
+  },
 }
 
 return M
--- .config/nvim_config/init.lua
+++ .config/nvim_config/init.lua
@@ -1,4 +1,5 @@
+vim.g.dashboard_disable_at_vimenter = 1
 vim.api.nvim_set_keymap("n", ":", ";", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", ";", ":", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>e", ":lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>rn', ":lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
