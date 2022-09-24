4f06b0f0aac2f8792c4d6a6082e98360cac753f4
Daze <dazehere@yandex.com>
2022-09-24 10:31:58 +0545

    feat: Migrate to fish shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+# Start X at login
+if status --is-interactive
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec startx -- -keeptty
+  end
+end
+
+set fish_greeting
+zoxide init fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
+
+# Alias
+alias py="python3"
+alias vim="nvim"
+alias vi="nvim"
+alias c="clear"
+alias top="htop"
+alias po="poetry"
+alias q="exit"
+alias dc="docker-compose"
+alias dk="docker"
+alias gs="git status"
+alias gch="git checkout"
+alias ls="exa"
+alias la="exa -la"
+alias sync='osync.sh ~/.config/osync/sync.conf'
+alias yay="paru --color always"
+alias du="du -h"
+alias tmux="TERM=screen-256color-bce /usr/bin/tmux"
+alias rm="trash"
+alias space="dust -d 2"
+alias tma="tmux attach -t"
