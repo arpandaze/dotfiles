ac3794ef7d045463fb383969e4c8ce4848759368
Daze <dazehere@yandex.com>
2023-02-12 22:32:18 +0545

    fix: Nvim dashboard fixed

--- .config/nvim/lua/daze/config/dashboard.lua
+++ .config/nvim/lua/daze/config/dashboard.lua
@@ -1,53 +1,63 @@
 local home = os.getenv("HOME")
-local dash = require("dashboard")
-
-dash.custom_header = {
-	"",
-	"",
-	" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-	" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-	" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-	" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-	" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-	"",
-	"",
-}
-dash.custom_center = {
-	{
-		icon = "  ",
-		desc = "Find  File                              ",
-		action = "Telescope find_files",
-		shortcut = "SPC f f",
+require("dashboard").setup({
+	theme = "doom",
+	config = {
+		header = {
+			"",
+			"",
+			" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+			" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+			" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+			" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+			" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+			" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+			"",
+			"",
+		},
+		center = {
+			{
+				icon = "  ",
+				desc = "Find  File",
+				action = "Telescope find_files",
+				-- key = "f",
+				key = "SPC f f",
+			},
+			{
+				icon = "  ",
+				desc = "Find  word",
+				action = "Telescope live_grep",
+				-- key = "f",
+				key = "SPC f w",
+			},
+			{
+				icon = "  ",
+				desc = "Recent session",
+				-- key = "s",
+				key = "SPC s l",
+				action = "SessionLoad",
+			},
+			{
+				icon = "  ",
+				desc = "Recently opened files",
+				action = "DashboardFindHistory",
+				-- key = "f",
+				key = "SPC f h",
+			},
+			{
+				icon = "  ",
+				desc = "Themes",
+				action = "Telescope colorscheme",
+				-- key = "t",
+				key = "SPC t h",
+			},
+			{
+				icon = "  ",
+				desc = "Open Personal dotfiles",
+				action = "Telescope dotfiles path=" .. home .. ".config/nvim",
+				-- key = "f",
+				key = "SPC f d",
+			},
+		},
+		footer = {},
 	},
-	{
-		icon = "  ",
-		desc = "Find  word                              ",
-		action = "Telescope live_grep",
-		shortcut = "SPC f w",
-	},
-	{
-		icon = "  ",
-		desc = "Recent session                          ",
-		shortcut = "SPC s l",
-		action = "SessionLoad",
-	},
-	{
-		icon = "  ",
-		desc = "Recently opened files                   ",
-		action = "DashboardFindHistory",
-		shortcut = "SPC f h",
-	},
-	{
-		icon = "  ",
-		desc = "Themes                                  ",
-		action = "Telescope colorscheme",
-		shortcut = "SPC t h",
-	},
-	{
-		icon = "  ",
-		desc = "Open Personal dotfiles                  ",
-		action = "Telescope dotfiles path=" .. home .. ".config/nvim",
-		shortcut = "SPC f d",
-	},
-}
+})
