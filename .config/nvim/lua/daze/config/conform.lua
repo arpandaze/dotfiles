1dbe4a469f270f099eda24d8ae69ff0705af7d51
Arpan Koirala <dazehere@yandex.com>
2023-11-22 05:59:48 +0545

    chore: indent-blankline config update and switch to conform for formatting

---
+++ .config/nvim/lua/daze/config/conform.lua
@@ -0,0 +1,17 @@
+local conform = require("conform")
+
+conform.setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+
+		-- Use a sub-list to run only the first available formatter
+		javascript = { "prettier" },
+
+		json = { "prettier" },
+
+		rust = { "rustfmt" },
+	},
+})
--- .config/nvim/lua/daze/config/indent-blankline.lua
+++ .config/nvim/lua/daze/config/indent-blankline.lua
@@ -1,24 +1,12 @@
-local blankline = require("indent_blankline")
+local blankline = require("ibl")
 
 local options = {
-	indentLine_enabled = 1,
-	filetype_exclude = {
-		"help",
-		"terminal",
-		"alpha",
-		"packer",
-		"lspinfo",
-		"TelescopePrompt",
-		"TelescopeResults",
-		"mason",
-		"dashboard",
-		"",
+  scope = { enabled = false },
+	exclude = {
+		filetypes = {
+			"dashboard",
+		},
 	},
-	buftype_exclude = { "terminal" },
-	show_trailing_blankline_indent = false,
-	show_first_indent_level = false,
-	show_current_context = true,
-	show_current_context_start = false,
 }
 
 blankline.setup(options)
