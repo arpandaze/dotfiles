a0601a4faee83a98036c7edc2dd08a1160a04fb0
Arpan Koirala <arpandaze@gmail.com>
2024-12-21 13:03:31 +0545

    feat: use ra multiplex for rust

--- .config/nvim/lua/daze/config/lsp-zero.lua
+++ .config/nvim/lua/daze/config/lsp-zero.lua
@@ -1,15 +1,26 @@
 local lsp = require("lsp-zero").preset("recommended")
 
 lsp.setup_nvim_cmp({
-    preselect = 'none',
-    completion = { completeopt = 'menu,menuone,noinsert,noselect' }
+	preselect = "none",
+	completion = { completeopt = "menu,menuone,noinsert,noselect" },
 })
 
 require("lspconfig").dartls.setup({
-    cmd = { "dart", "language-server", "--protocol=lsp" }
+	cmd = { "dart", "language-server", "--protocol=lsp" },
 })
 
--- lsp.setup_servers({ 'dartls', force = true })
+require("lspconfig").rust_analyzer.setup({
+	cmd = vim.lsp.rpc.connect("127.0.0.1", 27631),
+	settings = {
+		["rust-analyzer"] = {
+			lspMux = {
+				version = "1",
+				method = "connect",
+				server = "rust-analyzer",
+			},
+		},
+	},
+})
 
 lsp.setup()
 
